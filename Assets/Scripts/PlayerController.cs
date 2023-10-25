4f9350236d35dbcd05ffa54238145f936a86bf19
Mjones090 <145160703+Mjones090@users.noreply.github.com>
2023-10-25 23:18:15 +0100

    Vector3 to move forward

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
       
     {
         // Move player forward
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward);
 
     }
 }
