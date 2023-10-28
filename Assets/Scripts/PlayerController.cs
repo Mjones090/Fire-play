2fdd7313a09cc438e496a0d6e1dfa24bae236409
Mjones090 <145160703+Mjones090@users.noreply.github.com>
2023-10-28 15:45:50 +0100

    Fuel type 1 can be thrown by player towards fire

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -45,7 +45,11 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
         }
 
-
+        { if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // launch a piece of fuel for the fire from the player
+                Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            } }
 
 
         horizontalInput = Input.GetAxis("Horizontal");
@@ -54,7 +58,7 @@ public class PlayerController : MonoBehaviour
         //Move player forward and backwards based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
-        //Roate player left and right based on horizontal input
+        //Rotate player left and right based on horizontal input
         transform.Translate(Vector3.right * speed * horizontalInput *Time.deltaTime);
 
     }
